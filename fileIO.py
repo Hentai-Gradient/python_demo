8faf0aa46eeecc698dd54d799b1fa2d591cc219e
Maxiaoshi <maxiaoshi@Maxiaoshis-MBP.lan>
2016-09-07 00:29:19 +0800

    python demo

---
+++ fileIO.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# 打开一个文件
+import time
+
+fo = open("/Users/maxiaoshi/PycharmProjects/untitled/foo.txt", "r+")
+string = fo.readline()
+findCount = {}
+lines = 0
+print time.localtime(time.time())
+while 1:
+    line = fo.readline()
+    lines += 1
+    if not line:
+        break
+    if line.find("select") < 0:
+        continue
+    keyword = line[line.index("from") + 5:][:line[line.index("from") + 5:].index(" ")]
+    count = findCount.get(keyword, 0)
+    findCount[keyword] = count + 1
+
+print findCount
+print lines
+print time.localtime(time.time())
+fo.close()
---
+++ inputIO.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+string = raw_input("请输入：")
+print "你输入的内容是: ", string
